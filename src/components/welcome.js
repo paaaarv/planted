57b856078c1cd1baea216365f7db862971d874e5
paaaarv <pkumar@trinity.edu>
2020-06-02 19:37:03 -0500

    added welcome page, bootstrap

---
+++ src/components/welcome.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+export default class Welcome extends React.Component{
+
+
+
+  render(){
+    return(
+      <div>
+        <h1> w e l c o m e </h1>
+
+        <h4> planted -- a way for you to keep track of the little friends you've brought into your home!</h4>
+      </div>
+    )
+  }
+
+
+
+}
