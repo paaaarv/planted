23a40f0ae6cd9d97740efa78d9da5d1b91e8a458
paaaarv <pkumar@trinity.edu>
2020-06-10 13:27:28 -0500

    added state to form

--- src/components/form.js
+++ src/components/form.js
@@ -6,13 +6,27 @@ import React from 'react';
 export default class Form extends React.Component{
 
 
-
+  constructor(props){
+    super(props)
+    this.state={
+      name: '',
+      sun: '',
+      water: '',
+      soil: '',
+      fertilize: '',
+      repot: '',
+      propagate: '',
+      flowering: false,
+      poisonous: false,
+      additional: ''
+    }
+  }
 
 
 render() {
   return(
       <div>
-      </div> 
+      </div>
   )
 }
 }
