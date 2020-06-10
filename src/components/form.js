10b13c5a8134b885005e95e5ac666a445cce8db0
paaaarv <pkumar@trinity.edu>
2020-06-10 14:56:25 -0500

    added underline to form and padding

--- src/components/form.js
+++ src/components/form.js
@@ -39,21 +39,29 @@ render() {
       <label> plant name: </label>
       <input name="name" type='text' value={this.state.name} onChange={this.onChange}/>
       </Row>
+      <br/>
       <Row>
       <label> light: </label>
       <input name='light' type='text' value={this.state.sun} onChange={this.onChange}/>
       </Row>
+            <br/>
       <Row>
       <label> water: </label>
       <input name='water' type='text' value={this.state.water} onChange={this.onChange}/>
       </Row>
+            <br/>
       <Row>
       <label> soil: </label>
       <input name='soil' type='text' value={this.state.soil} onChange={this.onChange}/>
       </Row>
+            <br/>
       <Row>
       <label> fertilize: </label>
       <input name='fertilize' type='text' value={this.state.fertilize}/>
+      </Row>
+            <br/> 
+      <Row>
+      <input value="add plant!" type ='submit'></input>
       </Row>
       </form>
       </Container>
