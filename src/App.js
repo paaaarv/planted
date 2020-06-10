516259666855d3797583e65f78e1973bac48dee1
paaaarv <pkumar@trinity.edu>
2020-06-10 13:16:02 -0500

    added columns to list, background image

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/welcome.js'
-
+import List from './components/list.js'
 function App() {
   return (
     <div className="App">
       <Welcome/>
+      <List/> 
     </div>
   );
 }
--- src/components/list.js
+++ src/components/list.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import Container from 'react-bootstrap/container';
+import Col from 'react-bootstrap/col';
+import Row from 'react-bootstrap/row';
 
 
 
@@ -11,6 +13,17 @@ export default class List extends React.Component{
   render(){
     return(
       <div>
+      <Container fluid={true}>
+      <Row>
+      <Col xs={4} >name </Col>
+      <Col> || </Col>
+      <Col>sun </Col>
+      <Col> || </Col>
+      <Col>water </Col>
+      <Col> || </Col> 
+      <Col>fertilize</Col>
+      </Row>
+      </Container>
       </div>
     )
   }
