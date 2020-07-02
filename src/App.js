798da9f4fae3fea5487924c50b45f1d1d8517f65
paaaarv <pkumar@trinity.edu>
2020-07-01 20:42:07 -0500

    bio component on home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Welcome from './components/welcome.js'
+import Home from './components/home.js'
 import List from './components/list.js'
 import Form from './components/form.js'
 import Tab from './components/tab.js'
@@ -11,6 +11,7 @@ function App() {
   return (
     <Router>
     <Tab/>
+      <Route exact path='/' component={Home}/>
       <Route exact path='/add' component={Form}/>
       <Route exact path= '/plants' component={List}/>
     </Router>
--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,21 @@
 import React from 'react';
-
-
+import { connect } from 'react-redux'
+import Bio from './bio.js'
 
 class Home extends React.Component{
 
 
 
   render(){
+    const plants = this.props.plants.map(plant => <div> <Bio name={plant.name} sun={plant.sun}/> </div>)
     return(
       <div>
         <h1> w e l c o m e </h1>
 
         <h4> planted -- a way for you to keep track of the little friends you've brought into your home!</h4>
-
+          <div>
+          {plants}
+          </div>
         <br/> <br/>
       </div>
     )
