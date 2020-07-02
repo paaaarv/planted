1ab290f15c0179efaf7cbae4342df5a34ad1a8f4
paaaarv <pkumar@trinity.edu>
2020-07-01 20:21:49 -0500

    added bio page for individual plant

--- src/components/bio.js
+++ src/components/bio.js
@@ -11,7 +11,16 @@ export default class Bio extends React.Component{
   render(){
     return(
       <div>
-      
+          <h1> {this.props.name}</h1>
+          <div>
+          <h3>{this.props.sun} </h3>
+          </div>
+          <div>
+          <h3>{this.props.water} </h3>
+          </div>
+          <div>
+          <h3>{this.props.fertilize} </h3>
+          </div>
       </div>
 
 
