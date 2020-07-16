305351be2dd3592da393a21095dc727a242c6b87
paaaarv <pkumar@trinity.edu>
2020-07-16 16:33:25 -0500

    added login page

---
+++ src/components/login.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+
+
+export default class Login extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state= {
+      username:'',
+      password: ''
+    }
+  }
+
+  render(){
+    return(
+
+      
+    )
+  }
+
+}
